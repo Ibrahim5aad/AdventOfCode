ddbd2f88ed756ef758f4c2fbeb105d9e9b7bb8f9
Ibrahim Saad <ibrahim.saad@xbim.net>
2022-12-04 15:03:45 +0200

    day01: calories counting

---
+++ AdventOfCode.AoC22/Day01/CalorieCounting.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode.AoC22.Day01
+{
+    internal class CalorieCounting
+    {
+        public static (int, int) ElfWithMaxCalories()
+        {
+            var caloriesPerElf = GetCaloriesPerElf();
+
+            return caloriesPerElf.MaxBy(kvp => kvp.Item2);
+        }
+
+        public static int MaxCaloriesByTopThreeElfs()
+        {
+            var caloriesPerElf = GetCaloriesPerElf();
+
+            return caloriesPerElf.OrderByDescending(kvp => kvp.Item2).Take(3).Sum(e => e.Item2);
+        }
+
+        private static IEnumerable<(int, int)> GetCaloriesPerElf()
+        {
+            var data = File.ReadAllLines("Day01/input.txt");    
+            var current = new List<int>();
+            var i = 1;
+
+            foreach (var line in data)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    current.Add(int.Parse(line));
+                    continue;
+                }
+
+                yield return (i, current.Sum());
+                current = new List<int>();
+                i++;
+            }
+        }
+
+    }
+}
---
+++ AdventOfCode.AoC22/Program.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.AoC22.Day01;
+
+namespace AdventOfCode.AoC22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // --- Day 1: Calorie Counting ---
+
+            var maxElf = CalorieCounting.ElfWithMaxCalories();
+            Console.WriteLine($"Elf number {maxElf.Item1} has {maxElf.Item2} cals.");
+
+            var calsByTop3Elfs = CalorieCounting.MaxCaloriesByTopThreeElfs();
+            Console.WriteLine($"Top three Elfs has {calsByTop3Elfs} cals.");
+
+            // -------------------------------------------------- 
+
+
+            Console.ReadLine();
+        }   
+    }
+}
